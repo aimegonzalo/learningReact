373633a6eaaae1d7006ab0d3c50b998a6b442349
Gonzalo Aime <aimegonzalo@gmail.com>
2026-06-16 11:36:54 -0300

    agregado fetching de la imagen

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,31 +1,56 @@
 import { useEffect, useState } from "react";
 
 const RANDOM_FACT_ENDPOINT = "https://catfact.ninja/fact";
-// const RANDOM_IMAGE_ENDPOINT = `https://cataas.com/cat/says/${splittedFact}?fontSize=50&fontColor=red&json=true`;
+const getRandomImageEndpoint = (firstThreeWords) =>
+  `https://cataas.com/cat/says/${firstThreeWords}?fontSize=50&fontColor=red&json=true`;
 
 export function App() {
   const [randomFact, setRandomFact] = useState();
-  // const [randomImage, setRandomImage] = useState();
+  const [randomImage, setRandomImage] = useState();
+
+  const handleRandomFact = () => {
+    fetch(RANDOM_FACT_ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setRandomFact(fact);
+      });
+  };
 
   useEffect(() => {
     fetch(RANDOM_FACT_ENDPOINT)
       .then((res) => res.json())
       .then((data) => {
-        const {fact} = data
+        const { fact } = data;
         setRandomFact(fact);
-      
-        const firstThreeWords = fact.split(' ',3).join(' ')
-        console.log(firstThreeWords)
       });
-      
-      
-    
   }, []);
+
+  useEffect(() => {
+    if (!randomFact) return;
+
+    const firstThreeWords = randomFact.split(" ", 3).join(" ");
+    console.log(firstThreeWords);
+
+    fetch(getRandomImageEndpoint(firstThreeWords))
+      .then((res) => res.json())
+      .then((data) => {
+        const { url } = data;
+        setRandomImage(url);
+      });
+  }, [randomFact]);
 
   return (
     <>
+      <button onClick={handleRandomFact}>Get a Random Fact</button>
       <h1>Random Cats Facts App</h1>
       {randomFact && <p>{randomFact}</p>}
+      {randomImage && (
+        <img
+          src={randomImage}
+          alt="A cat image generated via cataas endpoint"
+        />
+      )}
     </>
   );
 }
